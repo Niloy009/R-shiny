f9e8a00966edd80454478afea2ba490cc1836162
Niloy009 <niloysaha.0009@gmail.com>
2024-03-09 13:12:16 +0100

    second commit

---
+++ practice-r-shiny/play_golf.R
@@ -0,0 +1,108 @@
+# Import libraries
+library(shiny)
+library(shinythemes)
+library(data.table)
+library(RCurl)
+library(randomForest)
+
+# Read data
+weather <- read.csv("/home/niloy/Desktop/R-Shiny-Mainz/practice-r-shiny/weather-weka.csv")
+weather$play <- as.factor(weather$play)
+weather$outlook <- as.factor(weather$outlook)
+weather$windy <- as.factor(weather$windy)
+# Build model
+model <- randomForest(play ~ ., data = weather, ntree = 500, mtry = 4, importance = TRUE)
+summary(model)
+
+
+#User interface                   
+ui <- fluidPage(theme = shinytheme("united"),
+                
+                # Page header
+                headerPanel('Play Golf?'),
+                
+                # Input values
+                sidebarPanel(
+                  HTML("<h3>Input parameters</h3>"),
+                  
+                  selectInput("outlook", label = "Outlook:", 
+                              choices = list("Sunny" = "sunny", "Overcast" = "overcast", "Rainy" = "rainy"), 
+                              selected = "Rainy"),
+                  sliderInput("temperature", "Temperature:",
+                              min = 64, max = 86,
+                              value = 70),
+                  sliderInput("humidity", "Humidity:",
+                              min = 65, max = 96,
+                              value = 90),
+                  selectInput("windy", label = "Windy:", 
+                              choices = list("Yes" = "TRUE", "No" = "FALSE"), 
+                              selected = "TRUE"),
+                  
+                  actionButton("submitbutton", "Submit", class = "btn btn-primary")
+                ),
+                
+                mainPanel(
+                  tags$label(h3('Status/Output')), # Status/Output Text Box
+                  verbatimTextOutput('contents'),
+                  tableOutput('tabledata') # Prediction results table
+                  
+                )
+)
+
+
+# Server
+
+server <- function(input, output, session) {
+  
+  # Input Data
+  datasetInput <- reactive({  
+    
+    # outlook,temperature,humidity,windy,play
+    df <- data.frame(
+      Name = c("outlook",
+               "temperature",
+               "humidity",
+               "windy"),
+      Value = as.character(c(input$outlook,
+                             input$temperature,
+                             input$humidity,
+                             input$windy)),
+      stringsAsFactors = FALSE)
+    
+    play <- 'play'
+    df <- rbind(df, play)
+   
+    input <- transpose(df)
+    
+    write.table(input,"input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
+    
+    test <- read.csv(paste("input", ".csv", sep=""), header = TRUE)
+    print(test)
+    test$outlook <- factor(test$outlook, levels = c("overcast", "rainy", "sunny"))
+    test$windy <- factor(test$windy, levels = c("TRUE", "FALSE"))
+    
+    Output <- data.frame(Prediction=predict(model,test), round(predict(model,test,type="prob"), 3))
+    print(Output)
+    
+  })
+  
+  # Status/Output Text Box
+  output$contents <- renderPrint({
+    if (input$submitbutton>0) { 
+      isolate("Calculation complete.") 
+    } else {
+      return("Server is ready for calculation.")
+    }
+  })
+  
+  # Prediction results table
+  output$tabledata <- renderTable({
+    if (input$submitbutton>0) { 
+      isolate(datasetInput()) 
+    } 
+  })
+  
+}
+
+# Create the shiny app
+shinyApp(ui = ui, server = server)
